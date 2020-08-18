24f7eff42323fc0a1a88ed19ea5679460e0f973b
Johanna Valeria Escudero <37539224+johiescudero@users.noreply.github.com>
2020-08-18 17:15:26 -0300

    changes on controllers
    using repositories

--- src/main/java/com/asistente/apiservice/controllers/FinalTestController.java
+++ src/main/java/com/asistente/apiservice/controllers/FinalTestController.java
@@ -1,5 +1,24 @@
 package com.asistente.apiservice.controllers;
 
+import java.util.List;
+
+import com.asistente.apiservice.models.FinalTest;
+import com.asistente.apiservice.repository.FinalTestRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FinalTestController {
+
+    @Autowired
+    private FinalTestRepository repositorio;
     
+    /**Retorna el conjunto total de finales registrados en la base de datos */
+    @GetMapping("/finaltests")
+     public List<FinalTest> listar(){
+       return repositorio.findAll();
+       
+     }
 }
--- src/main/java/com/asistente/apiservice/controllers/GoalController.java
+++ src/main/java/com/asistente/apiservice/controllers/GoalController.java
@@ -1,5 +1,25 @@
 package com.asistente.apiservice.controllers;
 
+import java.util.List;
+
+import com.asistente.apiservice.models.SubGoal;
+import com.asistente.apiservice.repository.GoalRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class GoalController {
+
+    @Autowired
+    private GoalRepository repositorio;
+
+    /**Retorna el conjunto total de objetivos registrados en la base de datos */
+    @GetMapping("/goals")
+     public List<SubGoal> listar(){
+       return repositorio.findAll();
+       
+     }
     
 }
