eb0ba7a0d529d925726ba87737fb22796d539d1d
Johanna Valeria Escudero <37539224+johiescudero@users.noreply.github.com>
2020-08-18 21:25:40 -0300

    Changes on controllers
    último commit del día

--- src/main/java/com/asistente/apiservice/controllers/FinalTestController.java
+++ src/main/java/com/asistente/apiservice/controllers/FinalTestController.java
@@ -6,7 +6,12 @@ import com.asistente.apiservice.models.FinalTest;
 import com.asistente.apiservice.repository.FinalTestRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,4 +26,21 @@ public class FinalTestController {
        return repositorio.findAll();
        
      }
+
+     /**POST : Añadir nuevo final */
+    @PostMapping
+    public void insertar(@RequestBody FinalTest newFinalTest) {
+      repositorio.save(newFinalTest);
+        
+    }
+    /**PUT : Actualizar un final */
+    @PutMapping
+    public void modificar(@RequestBody FinalTest user){
+      repositorio.save(user);
+    }
+    /**DELETE : Eliminar un final con el id */
+    @DeleteMapping(value = "/{id}")
+    public void eliminar(@PathVariable ("id") Integer id){
+      repositorio.deleteById(id);
+    }
 }
--- src/main/java/com/asistente/apiservice/controllers/GoalController.java
+++ src/main/java/com/asistente/apiservice/controllers/GoalController.java
@@ -6,7 +6,12 @@ import com.asistente.apiservice.models.SubGoal;
 import com.asistente.apiservice.repository.GoalRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +26,21 @@ public class GoalController {
        return repositorio.findAll();
        
      }
+     /**POST : Añadir nuevo subgoal */
+    @PostMapping
+    public void insertar(@RequestBody SubGoal newSubGoal) {
+      repositorio.save(newSubGoal);
+        
+    }
+    /**PUT : Actualizar un subgoal */
+    @PutMapping
+    public void modificar(@RequestBody SubGoal subGoal){
+      repositorio.save(subGoal);
+    }
+    /**DELETE : Eliminar un subgoal con el id */
+    @DeleteMapping(value = "/{id}")
+    public void eliminar(@PathVariable ("id") Integer id){
+      repositorio.deleteById(id);
+    }
     
 }
