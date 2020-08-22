6ffdbdeaa3b982c745f2e4c87b56d7c6c5384877
Johanna Valeria Escudero <37539224+johiescudero@users.noreply.github.com>
2020-08-22 19:55:27 -0300

    Create GoalService.java

---
+++ src/main/java/com/asistente/apiservice/services/GoalService.java
@@ -0,0 +1,34 @@
+package com.asistente.apiservice.services;
+
+import java.util.List;
+
+import com.asistente.apiservice.models.SubGoal;
+import com.asistente.apiservice.repository.GoalRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class GoalService {
+    
+    @Autowired
+    private GoalRepository goalRepository;
+
+    /** Retorna el conjunto total de objetivos registrados en la base de datos */
+    public List<SubGoal> getGoals(){
+      return goalRepository.findAll();
+    }
+     /**POST : Añadir nuevo subgoal */
+    public void addGoal(SubGoal newSubGoal) {
+        goalRepository.save(newSubGoal);
+        
+    }
+    /**PUT : Actualizar un subgoal */
+    public void updateGoal(SubGoal subGoal){
+        goalRepository.save(subGoal);
+    }
+    /**DELETE : Eliminar un subgoal con el id */
+    public void deleteGoal(Integer id){
+        goalRepository.deleteById(id);
+    }
+}
