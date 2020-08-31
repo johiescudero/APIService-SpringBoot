3a955b435ade53d4f7026e97df349d9ab279a3cc
Johanna Valeria Escudero <37539224+johiescudero@users.noreply.github.com>
2020-08-30 22:15:59 -0300

    Update UserController.java
    Cifra contraseña

--- src/main/java/com/asistente/apiservice/controllers/UserController.java
+++ src/main/java/com/asistente/apiservice/controllers/UserController.java
@@ -6,6 +6,9 @@ import com.asistente.apiservice.models.Users;
 import com.asistente.apiservice.services.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +24,8 @@ public class UserController {
     
     @Autowired
     private UserService userService; 
+    @Autowired
+	private BCryptPasswordEncoder encoder;
     
     /**GET : Retorna el conjunto total de usuarios registrados en la base de datos */
     @GetMapping(value = "/all")
@@ -30,10 +35,17 @@ public class UserController {
     }
     /**POST : Añadir nuevo usuario */
     @PostMapping(value = "/add")
-    public Users addUser(@RequestBody Users newUsers) {
-       return userService.addUser(newUsers);
-        
+    public ResponseEntity<Users> addUser(@RequestBody Users newUser) {
+        String password = newUser.getPassword();
+        newUser.setPassword(encoder.encode(password));
+        Users createdUser = userService.addUser(newUser);
+        if (createdUser !=null)
+            return new ResponseEntity<Users>(createdUser,HttpStatus.OK);
+        else
+            return new ResponseEntity<Users>(createdUser,HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+
     /**PUT : Actualizar un usuario */
     @PutMapping(value = "/update/{id}")
     public void updateUser(@RequestBody Users updUser){
