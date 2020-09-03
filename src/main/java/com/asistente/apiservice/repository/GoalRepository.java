5dd0becde5fe5de10f910b9a4023e3aa39fecf31
Johanna Valeria Escudero <37539224+johiescudero@users.noreply.github.com>
2020-09-02 21:12:13 -0300

    Update GoalRepository.java

--- src/main/java/com/asistente/apiservice/repository/GoalRepository.java
+++ src/main/java/com/asistente/apiservice/repository/GoalRepository.java
@@ -1,6 +1,8 @@
 package com.asistente.apiservice.repository;
 
 import java.util.List;
+
+import com.asistente.apiservice.models.Exam;
 import com.asistente.apiservice.models.Goal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,5 +11,5 @@ public interface GoalRepository extends JpaRepository<Goal,Integer> {
 
     //Select * FROM GOALS WHERE idFinal = id
     //Utilizado para a través de la configuración de Spring security obtener un usuario
-    List<Goal> findByIdFinal(int id);
+    List<Goal> findByExam(Exam Exam);
 }
